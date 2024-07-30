06a253a9e814e135f55adc27538c3d4715710c86
Kyle Zhang <kyleyzhang@gmail.com>
2024-07-30 12:24:48 -0400

    Add timer class

---
+++ src/timer.h
@@ -0,0 +1,22 @@
+#ifndef PEACEKEEPER_TIMER
+#define PEACEKEEPER_TIMER
+
+#include "types.h"
+#include <chrono>
+
+class Timer {
+private:
+    using clock_t = std::chrono::steady_clock;
+    using second_t = std::chrono::duration<double, std::ratio<1>>;    
+    std::chrono::time_point<clock_t> m_beg;
+public:
+    Timer() : m_beg(clock_t::now()) {}
+    void reset() {
+        m_beg = clock_t::now();
+    }
+    double elapsed() const {
+        return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+    }
+};
+
+#endif
