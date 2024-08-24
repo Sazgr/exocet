8a3e70a65213f6e042584b42b8b4849397c7fe87
Kyle Zhang <kyleyzhang@gmail.com>
2024-08-24 13:57:43 -0400

    Continuation history

--- src/move_order.h
+++ src/move_order.h
@@ -6,10 +6,18 @@
 
 struct Move_order {
     int history[12][64]{};
+    int* continuation;
     Move killer[128][2]{};
 
+    Move_order() {
+        continuation = new int[12 * 64 * 12 * 64];
+    }
+    ~Move_order() {
+        delete[] continuation;
+    }
     void reset() {
         std::memset(history, 0, sizeof(history));
+        std::memset(continuation, 0, sizeof(continuation));
         for (int i{}; i < 128; ++i) {
             killer[i][0] = Move{};
             killer[i][1] = Move{};
@@ -17,11 +25,23 @@ struct Move_order {
     }
 
     void history_update(Move move, int bonus) {
-       history[move.piece()][move.end()] += bonus - history[move.piece()][move.end()] * std::abs(bonus) / 1024;
+        history[move.piece()][move.end()] += bonus - history[move.piece()][move.end()] * std::abs(bonus) / 1024;
     }
 
     int history_score(Move move) {
         return history[move.piece()][move.end()];
+    }
+
+    void continuation_update(Move previous, Move current, int bonus) {
+        if (previous.is_null()) return;
+        int index = previous.piece() * 64 * 12 * 64 + previous.end() * 12 * 64 + current.piece() * 64 + current.end();
+        continuation[index] += bonus - continuation[index] * std::abs(bonus) / 1024;
+    }
+
+    int continuation_score(Move previous, Move current) {
+        if (previous.is_null()) return 0;
+        int index = previous.piece() * 64 * 12 * 64 + previous.end() * 12 * 64 + current.piece() * 64 + current.end();
+        return continuation[index];
     }
 
     void killer_update(Move move, int ply) {
