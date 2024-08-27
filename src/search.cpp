e610b2373d10afaf7d561e3b7f6b173960acb06c
Kyle Zhang <kyleyzhang@gmail.com>
2024-08-27 10:38:17 -0400

    History pruning

--- src/search.cpp
+++ src/search.cpp
@@ -167,6 +167,7 @@ int search(Position& position, Search_stack* ss, Search_data& sd, int depth, int
     for (int i{}; i < movelist.size(); ++i) {
         if (!position.is_legal(movelist[i])) continue;
         if (!is_root && best_score > -18000) {
+            if (movelist[i].captured() == 12 && depth < 5 && movelist[i].sortkey() < 15000 - 100 * depth) continue;
             if (movelist[i].captured() == 12 && !see(position, movelist[i], -50 * depth * depth)) continue;
         }
         position.make_move<true>(movelist[i], sd.nnue);
