5df895350fcc6257553c1c91a50e4c7bbb8e3444
Kyle Zhang <kyleyzhang@gmail.com>
2024-08-27 13:02:23 -0400

    Check extensions

--- src/search.cpp
+++ src/search.cpp
@@ -148,6 +148,7 @@ int search(Position& position, Search_stack* ss, Search_data& sd, int depth, int
             return (abs(score) > 18000 ? beta : score);
         }
     }
+    if (in_check) ++depth;
     if (!is_pv && depth >= 6 && !(tt_hit && !entry.move().is_null())) depth--;
     position.generate_stage<all>(movelist);
     for (int i{}; i < movelist.size(); ++i) {
