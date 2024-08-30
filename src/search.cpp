ddbc5bca9de4ddc494805d058a4671bbe70f4026
Kyle Zhang <kyleyzhang@gmail.com>
2024-08-30 13:20:56 -0400

    More LMP

--- src/search.cpp
+++ src/search.cpp
@@ -209,7 +209,7 @@ int search(Position& position, Search_stack* ss, Search_data& sd, int depth, int
         position.make_move<true>(movelist[i], sd.nnue);
         ss->move = movelist[i];
         bool gives_check = position.check();
-        if (depth < 8 && !in_check && !gives_check && legal_moves >= (4 + depth * depth) * (improving + 1)) {
+        if (depth < 8 && !in_check && !gives_check && legal_moves >= (2 + depth * depth) * (improving + 1)) {
             position.undo_move<true>(movelist[i], sd.nnue);
             break;
         }
