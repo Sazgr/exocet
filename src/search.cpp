5b48f16ca68b8070e9ac9d8bb37d4bdf1c27e828
Kyle Zhang <kyleyzhang@gmail.com>
2024-09-06 18:22:38 -0400

    Cutnode LMR

--- src/search.cpp
+++ src/search.cpp
@@ -223,6 +223,7 @@ int search(Position& position, Search_stack* ss, Search_data& sd, int depth, int
             reduction = static_cast<int>(0.5 + std::log(legal_moves) * std::log(depth) / 3.0);
             if (is_pv) --reduction;
             if (!improving) ++reduction;
+            if (cutnode) ++reduction;
             if (movelist[i].captured() != 12) --reduction;
             if (movelist[i].captured() == 12) reduction -= std::clamp(static_cast<int>(movelist[i].sortkey() - 15000) / 400, -2, 2);
             reduction = std::clamp(reduction, 0, depth - 2); //ensure that lmr reduction does not drop into quiescence search
