ffad04b4e73c607fc9a70a243cf3aa18749a5974
Kyle Zhang <kyleyzhang@gmail.com>
2024-09-12 11:48:45 -0400

    TT static eval adjustment

--- src/search.cpp
+++ src/search.cpp
@@ -152,6 +152,9 @@ int search(Position& position, Search_stack* ss, Search_data& sd, int depth, int
         return std::clamp(entry.score(), -18000, 18000);
     }
     int static_eval = position.static_eval(*sd.nnue);
+    if (tt_hit && (entry.type() == tt_exact || (entry.type() == tt_alpha && entry.score() <= static_eval) || (entry.type() == tt_beta && entry.score() >= static_eval))) {
+        static_eval = std::clamp(entry.score(), -18000, 18000);
+    }
     ss->static_eval = static_eval;
     int score = -20001;
     int best_score = -20001;
