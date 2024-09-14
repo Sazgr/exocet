8fdcb0f0f80b6561ee20a9a55f135a5baed05df8
Kyle Zhang <kyleyzhang@gmail.com>
2024-09-14 10:04:22 -0400

    Negative extensions

--- src/search.cpp
+++ src/search.cpp
@@ -255,6 +255,8 @@ int search(Position& position, Search_stack* ss, Search_data& sd, int depth, int
                 }
             } else if (singular_beta >= beta) {
                 return singular_beta;
+            } else if (entry.score() >= beta) {
+                extension = -1;
             }
         }
         position.make_move<true>(movelist[i], sd.nnue);
