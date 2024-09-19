59f03732a1206728e21f0460eb2d3d93a8dd5705
Kyle Zhang <kyleyzhang@gmail.com>
2024-09-19 13:03:51 -0400

    More IIR

--- src/search.cpp
+++ src/search.cpp
@@ -226,7 +226,7 @@ int search(Position& position, Search_stack* ss, Search_data& sd, int depth, int
         }
     }
     if (in_check) ++depth;
-    if (!is_pv && depth >= 6 && !(tt_hit && !entry.move().is_null())) depth--;
+    if (depth >= 6 && !(tt_hit && !entry.move().is_null())) depth--;
     position.generate_stage<all>(movelist);
     for (int i{}; i < movelist.size(); ++i) {
         if (tt_hit && movelist[i] == entry.move()) {
